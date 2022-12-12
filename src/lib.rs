b7f18ba2f47e3ca0478bb53bf6e15cca51b3b267
Benjamin Neff <benjamin@coding4coffee.ch>
2022-12-12 20:11:35 +0100

    Create aoc_solutions macro to select solutions

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,49 @@
 use std::{io::{BufRead, BufReader}, fs::File};
 
+use crate::day::AoCDay;
+
 mod day;
 
-mod y2022;
-
-pub fn run_solution_for(year: u16, day: u8, input: &mut impl BufRead) -> Result<(), &'static str> {
-    if day < 1 || day > 25 {
-        Err("Day needs to be between 1 and 25")
-    } else {
-        match year {
-            2022 => Ok(day::run_day(y2022::get_day(day, input)?)),
-            _ => Err("Year is not implemented yet"),
-        }
-    }
+mod y2022 {
+    pub mod day1;
 }
+
+macro_rules! aoc_solutions {
+    ($(($year:ident: $($day:ident),+)),+) => {
+        pub fn run_solution_for(year: u16, day: u8, input: &mut impl BufRead) -> Result<(), String> {
+            match year {
+                $(
+                    y if y == stringify!($year)[1..].parse::<u16>().unwrap() => {
+                        match day {
+                            $(
+                                d if d == stringify!($day)[3..].parse::<u8>().unwrap() => {
+                                    println!("Day {} {}: ", day, y);
+                                    Ok(day::run_day(<$year::$day::Solution as AoCDay>::with_input(input)))
+                                },
+                            )+
+                            _ => {
+                                let mut days: Vec<&str> = vec![];
+                                $(
+                                    days.push(&stringify!($day)[3..]);
+                                )+
+                                Err(format!("Day {} {} is not implemented yet, days with solutions in {}: {}", day, y, y, days.join(", ")))
+                            },
+                        }
+                    }
+                )+
+                _ => {
+                    let mut years: Vec<&str> = vec![];
+                    $(
+                        years.push(&stringify!($year)[1..]);
+                    )+
+                    Err(format!("Year {} is not implemented yet, years with solutions: {}", year, years.join(", ")))
+                },
+            }
+        }
+    };
+}
+
+aoc_solutions!((y2022: day1));
 
 pub fn get_input(year: u16, day: u8) -> Result<impl BufRead, &'static str> {
     let path = format!("input/{}/day{}.txt", year, day);
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use adventofcode::*;
 
-fn main() -> Result<(), &'static str> {
+fn main() -> Result<(), String> {
     let mut input = get_input(2022, 1).unwrap();
     run_solution_for(2022, 1, &mut input)
 }
