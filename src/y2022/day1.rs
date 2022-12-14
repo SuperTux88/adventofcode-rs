7afe02e4de82c3b6c08dae30d2e1b6915b4030ea
Benjamin Neff <benjamin@coding4coffee.ch>
2022-12-14 10:45:40 +0100

    Add pre-commit with fmt, check and clippy
    And format existing code

--- src/y2022/day1.rs
+++ src/y2022/day1.rs
@@ -3,13 +3,14 @@ use std::io::{self, BufRead};
 use crate::day::AoCDay;
 
 pub struct Solution {
-    elves_calories: Vec<u32>
+    elves_calories: Vec<u32>,
 }
 
 impl AoCDay for Solution {
     fn with_input(input: &mut impl BufRead) -> Self {
         let input_str = io::read_to_string(input).unwrap();
-        let elves = input_str.split("\n\n")
+        let elves = input_str
+            .split("\n\n")
             .map(|elf| elf.lines().map(|c| c.parse::<u32>().unwrap()));
         let mut elves_calories: Vec<u32> = elves.map(|elf| elf.sum()).collect();
         elves_calories.sort();
@@ -22,6 +23,11 @@ impl AoCDay for Solution {
     }
 
     fn part2(&self) -> String {
-        self.elves_calories.iter().rev().take(3).sum::<u32>().to_string()
+        self.elves_calories
+            .iter()
+            .rev()
+            .take(3)
+            .sum::<u32>()
+            .to_string()
     }
 }
