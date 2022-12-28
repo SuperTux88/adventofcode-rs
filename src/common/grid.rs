5323fcb5999234e95a4fcbebb27e19c1aa32951f
Benjamin Neff <benjamin@coding4coffee.ch>
2022-12-28 18:28:29 +0100

    Add helper method to parse a grid into a HashSet<IVec2>

--- src/common/grid.rs
+++ src/common/grid.rs
@@ -1,3 +1,40 @@
+use std::collections::HashSet;
+
+use glam::IVec2;
+
 pub mod directions;
 pub mod distance;
 pub mod walk;
+
+pub fn parse_set(lines: impl Iterator<Item = String>) -> HashSet<IVec2> {
+    lines
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .filter_map(move |(x, c)| match c {
+                    '#' => Some(IVec2::new(x as i32, y as i32)),
+                    _ => None,
+                })
+                .collect::<HashSet<IVec2>>()
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_set() {
+        let grid = ".#.\n..#\n###";
+        let parsed = parse_set(grid.lines().map(|s| s.to_string()));
+
+        assert_eq!(parsed.len(), 5);
+        assert!(parsed.contains(&IVec2::new(1, 0)));
+        assert!(parsed.contains(&IVec2::new(2, 1)));
+        assert!(parsed.contains(&IVec2::new(0, 2)));
+        assert!(parsed.contains(&IVec2::new(1, 2)));
+        assert!(parsed.contains(&IVec2::new(2, 2)));
+    }
+}
