513410358b3ea88ac448dfee0d1ea91e00307784
Benjamin Neff <benjamin@coding4coffee.ch>
2023-01-05 22:02:14 +0100

    Run check and clippy with all features
    This is to also check the wasm code, but also this needed to only enable
    the wasm feature check compile-error only on wasm32 target so clippy
    doesn't fail with it.

--- src/aoc/wasm.rs
+++ src/aoc/wasm.rs
@@ -4,22 +4,25 @@ use crate::Solutions;
 
 use super::results::Results;
 
-#[cfg(feature = "online")]
+#[cfg(all(feature = "online", target_arch = "wasm32"))]
 compile_error!("feature \"online\" cannot be enabled with \"wasm\" at the same time. Use \"...  --no-default-features --features wasm\" instead.");
-#[cfg(feature = "parallel")]
+#[cfg(all(feature = "parallel", target_arch = "wasm32"))]
 compile_error!("feature \"parallel\" cannot be enabled with \"wasm\" at the same time. Use \"...  --no-default-features --features wasm\" instead.");
 
 #[wasm_bindgen]
+#[allow(dead_code)]
 pub fn years() -> Vec<u16> {
     Solutions::years()
 }
 
 #[wasm_bindgen]
+#[allow(dead_code)]
 pub fn days_for_year(year: u16) -> Vec<u8> {
     Solutions::days_for_year(year)
 }
 
 #[wasm_bindgen]
+#[allow(dead_code)]
 pub fn run(year: u16, day: u8, part: &str, input: &str) -> WasmResults {
     Solutions::run(year, day, &part.parse().unwrap(), &mut input.as_bytes()).into()
 }
