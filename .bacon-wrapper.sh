f2873bd814b1b8a16b113a46a0e8e618576431aa
Benjamin Neff <benjamin@coding4coffee.ch>
2023-12-20 16:49:19 +0100

    Add bacon-wrapper bash script
    This is a wrapper script for bacon calling cargo commands. This allows
    to select only one day with "bacon -- --day 1", but fixes the problem,
    that different cargo commands need this information in a different
    format (run/test/bench) or some cargo commands don't take parameters
    like this at all (check/clippy). So it calls all the cargo commands with
    parameters which work for them.
    
    See: https://github.com/Canop/bacon/issues/122

---
+++ .bacon-wrapper.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+COMMAND=$1
+shift
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -y|--year)
+      YEAR="$2"
+      shift;;
+    -d|--day)
+      DAY="$2"
+      YEAR=${YEAR:-2023}
+      shift;;
+    -r|--release)
+      RELEASE="--release"
+      ;;
+    -*|--*)
+      echo "Unknown option $1" >&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+if [[ -n "$YEAR" ]]; then
+    RUN_ARGS="--year ${YEAR}"
+    TEST_ARGS="y${YEAR}"
+    BENCH_ARGS="${YEAR}"
+fi
+if [[ -n "$DAY" ]]; then
+    RUN_ARGS="${RUN_ARGS} --day ${DAY}"
+    TEST_ARGS="${TEST_ARGS}::day${DAY}::tests"
+    BENCH_ARGS="${BENCH_ARGS} day ${DAY}"
+fi
+COLOR="--color always"
+
+case "${COMMAND}" in
+    "check")
+        exec cargo check --all-targets ${COLOR}
+        ;;
+    "clippy")
+        exec cargo clippy --all-targets ${COLOR}
+        ;;
+    "test")
+        exec cargo test ${COLOR} -- ${COLOR} "${TEST_ARGS}"
+        ;;
+    "run")
+        exec cargo run ${COLOR} ${RELEASE} -- run ${RUN_ARGS}
+        ;;
+    "bench")
+        exec cargo run ${COLOR} ${RELEASE} -- bench ${RUN_ARGS}
+        ;;
+    "criterion")
+        exec cargo bench ${COLOR} --bench solutions -- ${COLOR} "${BENCH_ARGS}"
+        ;;
+    *)
+        echo "Unknown command" >&2
+        ;;
+esac
