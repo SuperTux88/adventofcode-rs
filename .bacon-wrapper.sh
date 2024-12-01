a29caa3853a0d8b3f0022ee229845c0e31de72a6
Benjamin Neff <benjamin@coding4coffee.ch>
2024-12-01 21:51:53 +0100

    Update bacon config to use the bacon-wrapper.sh less
    * Use extraneous_args for commands which can't use extra args passed to
      bacon.
    * Pass extra args to `cargo run` commands directly, which can parse the
      --day and --year, but keep the wrapper for tests and benchmarks, which
      require a different format as parameters.
    * Allow tests to run even with warnings.
    * Add some default keybinding to make sure they don't conflict with own
      ones.

--- .bacon-wrapper.sh
+++ .bacon-wrapper.sh
@@ -12,9 +12,6 @@ while [[ $# -gt 0 ]]; do
       DAY="$2"
       YEAR=${YEAR:-2024}
       shift;;
-    -r|--release)
-      RELEASE="--release"
-      ;;
     -*|--*)
       echo "Unknown option $1" >&2
       exit 1
@@ -24,32 +21,18 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [[ -n "$YEAR" ]]; then
-    RUN_ARGS="--year ${YEAR}"
     TEST_ARGS="y${YEAR}"
     BENCH_ARGS="${YEAR}"
 fi
 if [[ -n "$DAY" ]]; then
-    RUN_ARGS="${RUN_ARGS} --day ${DAY}"
     TEST_ARGS="${TEST_ARGS}::day${DAY}::tests"
     BENCH_ARGS="${BENCH_ARGS} day ${DAY}"
 fi
 COLOR="--color always"
 
 case "${COMMAND}" in
-    "check")
-        exec cargo check --all-targets ${COLOR}
-        ;;
-    "clippy")
-        exec cargo clippy --all-targets ${COLOR}
-        ;;
     "test")
         exec cargo test ${COLOR} -- ${COLOR} "${TEST_ARGS}"
-        ;;
-    "run")
-        exec cargo run ${COLOR} ${RELEASE} -- ${COLOR} run ${RUN_ARGS}
-        ;;
-    "bench")
-        exec cargo run ${COLOR} ${RELEASE} -- ${COLOR} bench ${RUN_ARGS}
         ;;
     "criterion")
         exec cargo bench ${COLOR} --bench solutions -- ${COLOR} "${BENCH_ARGS}"
